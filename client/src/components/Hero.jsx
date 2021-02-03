28309d75c663c03d83ded68d3becf987874ce33e
derekb123 <derekbutvin@gmail.com>
2021-02-02 23:50:14 -0500

    updated hero, about, skills components and styles

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
                 .
               </h1>
             </Fade>
-            <Fade left={true} duration={1200} delay={1000} distance='50px'>
+            <Fade left={true} duration={1700} delay={1500} distance='50px'>
               <h1 className="hero-title">
                 I'm a
                 <br />
@@ -32,12 +32,16 @@ const Hero = () => {
           <Col xs={1}>
           </Col>
           <Col>
-          <Fade right={true} duration={1200} delay={1000} distance='50px'>
+          <Fade right={true} duration={1700} delay={1500} distance='50px'>
               <img src={ heroImage } alt='this is a cowboy riding a computer mouse'/>
             </Fade>
           </Col>
         </Row>
-
+        <Row className='hero-arrow'>
+          <Fade bottom={true} duration={1200} delay={2400} distance='50px'>
+            <p><i className='arrow down'></i></p>
+          </Fade>
+        </Row>
       </Container>
     </section>
   )
