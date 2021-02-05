cb53c059abda7304956bad2a4833b813b796b5c3
derekb123 <derekbutvin@gmail.com>
2021-02-05 17:23:55 -0500

    changed flip effect on projects

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -9,10 +9,10 @@ const Project = ({name, image, text, demo, demoPath, githubPath}) => {
   return (
     <Container className='project'>
         <Row className='projects-info'>
+          <Flip top={true} duration={1300} delay={400} distance='30px'>
             <Subtitle subtitle={name}/>
-            <Flip top={true} duration={1300} delay={400} distance='30px'>
-              <img className='projects-image' src={ image } alt='this is an amazing project'/>
-            </Flip>
+          </Flip>
+            <img className='projects-image' src={ image } alt='this is an amazing project'/>
             <Row className='projects-buttons'>
                 {demo &&
                    <Button variant='secondary' className='project-button' onClick={() => window.open(demoPath)}>demo</Button>
